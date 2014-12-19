d93bf503043167bea822bbcc48421b6a67729a75
Juergen Heil <juergen@meema.org>
2014-12-19 19:45:15 +0100

    completed apache/php setup

--- d-web02.meema.lan.pp
+++ d-web02.meema.lan.pp
@@ -1,11 +1,53 @@
 node 'd-web02.meema.lan' {
+    
+    # remove firewall
+    package { 'firewalld':
+        ensure => 'absent',
+    }
+    
+    
+    ##############################################################
+    # install base modules/packages
+    ##############################################################
+    
     include ntp
     include apache
-    apache::vhost { 'first.example.com':
-        port         => '80',
-        docroot      => '/var/www/first',
-        suphp_engine => 'off',
+    class { 'php': }
+    class {'::apache::mod::php':}
+    
+    ##############################################################
+    # vhost: yii-tutorial.meema.lan
+    ##############################################################
+    
+    # define some directories and files
+    $vhost_name = "yii-tutorial.meema.lan"
+    $vhost_root = "/var/www/${vhost_name}"
+    $vhost_log_root = "/var/www/${vhost_name}/logs"
+
+
+    # setting up vhost root directory and vhost log root
+    file { [$vhost_root, $vhost_log_root]:
+        ensure => 'directory',
+        owner  => 'apache',
+        group  => 'apache',
     }
+
+    apache::vhost { $vhost_name:
+        port            => '80',
+        docroot         => "${vhost_root}/docs",
+        logroot         => $vhost_log_root,
+        suphp_engine    => 'off',
+    }
+
+    # create index.php to test php availability
+    file { "${vhost_root}/docs/index.php":
+        ensure => 'present',
+        content => "<?php phpinfo(); ?>",
+    }
+
+    ##############################################################
+    # mysql-server
+    ##############################################################
 
     class { '::mysql::server':
         root_password    => 'eTcJ0708=',
@@ -39,4 +81,8 @@ node 'd-web02.meema.lan' {
             },
         }
     }
+    
+    ##############################################################
+    # ftp 
+    ##############################################################
 }
