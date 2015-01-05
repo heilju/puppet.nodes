181e25b874d9a3a338fef8353d89101e134b102e
Juergen Heil <juergen@meema.org>
2015-01-05 10:59:39 +0100

    dev host changed
    development vhost changed to sunpower
    added some php libs

--- d-web02.meema.lan.pp
+++ d-web02.meema.lan.pp
@@ -5,11 +5,11 @@ node 'd-web02.meema.lan' {
         ensure => 'absent',
     }
     
-    # install php mcrypt library
-    package { 'php-mcrypt':
+    # install some libraries
+    package { ['php-mcrypt', 'php-pdo', 'php-mysql', 'yum-utils']:
         ensure => 'present',
     }
-    
+
     ##############################################################
     # install base modules/packages
     ##############################################################
@@ -25,23 +25,18 @@ node 'd-web02.meema.lan' {
     ##############################################################
     
     # setting up vhost root directory and vhost log root
-    file { ["/var/www/laravel-tutorial.meema.lan", "/var/www/laravel-tutorial.meema.lan/docs", "/var/www/laravel-tutorial.meema.lan/logs"]:
+    file { ["/var/www/sunpower.meema.lan", "/var/www/sunpower.meema.lan/docs", "/var/www/sunpower.meema.lan/logs"]:
         ensure => 'directory',
         owner  => 'apache',
         group  => 'apache',
     }
 
-    apache::vhost { "laravel-tutorial.meema.lan":
+    apache::vhost { "sunpower.meema.lan":
         port            => '80',
-        docroot         => "/var/www/laravel-tutorial.meema.lan/docs/public",
-        logroot         => "/var/www/laravel-tutorial.meema.lan/logs",
+        docroot         => "/var/www/sunpower.meema.lan/docs/public",
+        logroot         => "/var/www/sunpower.meema.lan/logs",
+        override        => ['All'],
         suphp_engine    => 'off',
-    }
-
-    # create index.php to test php availability
-    file { "/var/www/laravel-tutorial.meema.lan/docs/index.php":
-        ensure => 'present',
-        content => "<?php phpinfo(); ?>",
     }
 
     ##############################################################
@@ -50,38 +45,55 @@ node 'd-web02.meema.lan' {
 
     class { '::mysql::server':
         root_password    => 'eTcJ0708=',
-        override_options => $override_options,
+        override_options => {
+            'mysqld' => {
+                'bind-address' => '0.0.0.0',
+            }
+        },
 
         databases => {
-            'somedb' => {
+            'sunpower' => {
                 ensure  => 'present',
                 charset => 'utf8',
             },
         },
 
         users => {
-            'someuser@localhost' => {
+            'admin@10.0.0.17' => {
                 ensure                   => 'present',
                 max_connections_per_hour => '0',
                 max_queries_per_hour     => '0',
                 max_updates_per_hour     => '0',
                 max_user_connections     => '0',
-                password_hash            => '*F3A2A51A9B0F2BE2468926B4132313728C250DBF',
+                password_hash            => '*A4A00DC274E651E5575A158F2F5AFF8FEBE6AB84',
+            },
+
+            'app_sunpower@localhost' => {
+                ensure                   => 'present',
+                max_connections_per_hour => '0',
+                max_queries_per_hour     => '0',
+                max_updates_per_hour     => '0',
+                max_user_connections     => '0',
+                password_hash            => '*A4A00DC274E651E5575A158F2F5AFF8FEBE6AB84',
             },
         },
 
         grants => {
-            'someuser@localhost/somedb.*' => {
+            'admin@10.0.0.17/sunpower.*' => {
+                ensure     => 'present',
+                options    => ['GRANT'],
+                privileges => ['ALL'],
+                table      => 'sunpower.*',
+                user       => 'admin@10.0.0.17',
+            },
+
+            'app_sunpower@localhost/sunpower.*' => {
                 ensure     => 'present',
                 options    => ['GRANT'],
                 privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
-                table      => 'somedb.*',
-                user       => 'someuser@localhost',
+                table      => 'sunpower.*',
+                user       => 'app_sunpower@localhost',
             },
         }
     }
-    
-    ##############################################################
-    # ftp 
-    ##############################################################
 }
